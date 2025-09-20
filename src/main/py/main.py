28895da7a7c61b84aa434b6206e9ad70a943c525
Steffen Kowalski <scipper@myscipper.de>
2025-09-20 21:46:35 +0200

    fixed pytest execution

--- src/main/py/main.py
+++ src/main/py/main.py
@@ -10,19 +10,22 @@ class Main:
         self._categories: List[Category] = []
 
     def start(self):
-        income = Category()
-        income.add_element(Element(1000, FinancialType.INCOME))
-        living = Category()
-        living.add_element(Element(350, FinancialType.COST))
+        incomes = Category()
+        incomes.add_element(Element("Salary", 1000, FinancialType.INCOME))
+        livings = Category()
+        livings.add_element(Element("Rent", 350, FinancialType.COST))
         loans = Category()
-        loans.add_element(Element(200, FinancialType.COST))
+        loans.add_element(Element("Car", 200, FinancialType.COST))
         savings = Category()
-        savings.add_element(Element(150, FinancialType.SAVING))
+        savings.add_element(Element("Vacation", 150, FinancialType.SAVING))
 
-        self._categories.append(income)
-        self._categories.append(living)
+        self._categories.append(incomes)
+        self._categories.append(livings)
         self._categories.append(loans)
         self._categories.append(savings)
+
+        for category in self._categories:
+            print(category.get_element_information())
 
         remaining_money = sum(category.get_sum() for category in self._categories)
         print("Remaining Money: " + str(remaining_money))
--- src/test/py/unit/DoNothingTest.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class MyCaseTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
